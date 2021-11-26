200900053a7f4f26778a271da6243fa972176be8
Mateusz <gyros.pl@gmail.com>
2021-11-26 20:19:22 +0100

    fixed typoo

--- Source/base_model_train.py
+++ Source/base_model_train.py
@@ -3,7 +3,7 @@ from base_model import Transformer
 from transformer_layers import auto_reg_loss, accuracy_function
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import tensorflow as tf
-
+import numpy as np
 def load_texts(path, word_limit = None):
   out_pt = []
   out_eng = []
@@ -27,7 +27,7 @@ def preprocess_data(eng, pt, tokenizer_eng, tokenizer_pt, batch_size = 32):
   dataset = dataset.batch(batch_size, True)
   return dataset
 
-def train_with_adam(train_dataset, eng_word_count, pt_word_count ):
+def train_base(train_dataset, eng_word_count, pt_word_count, optimizer = "Adam", **kwargs ):
   num_layers = 4
   d_model = 128
   dff = 512
@@ -38,11 +38,14 @@ def train_with_adam(train_dataset, eng_word_count, pt_word_count ):
                                   input_vocab_size=eng_word_count, target_vocab_size=pt_word_count,
                                   pe_input=1000, pe_target=1000, rate=dropout_rate)
 
-  transformer_model.compile("Adam", loss=auto_reg_loss, metrics=[accuracy_function])
-  history = transformer_model.fit(train_dataset, epochs=10)
+  transformer_model.compile(optimizer, loss=auto_reg_loss, metrics=[accuracy_function], run_eagerly = True)
+  history = transformer_model.fit(train_dataset, **kwargs)
 
 
-train_pt, train_eng = load_texts("../datasets/eng-pt/train.txt", 10000)
+train_pt, train_eng = load_texts("../datasets/eng-pt/train.txt", 70)
+
+#train_pt = train_pt[49:100]
+#train_eng = train_eng[49:100]
 
 tokenizer_eng = Tokenizer()
 tokenizer_pt = Tokenizer()
@@ -50,9 +53,10 @@ tokenizer_pt = Tokenizer()
 tokenizer_eng.fit_on_texts(train_eng)
 tokenizer_pt.fit_on_texts(train_pt)
 
-train_dataset = preprocess_data(train_eng,train_pt, tokenizer_eng, tokenizer_pt)
+train_dataset = preprocess_data(train_eng,train_pt, tokenizer_eng, tokenizer_pt, 1)
 
 eng_word_count = len(tokenizer_eng.word_index)+1
-pt_word_count = len(tokenizer_eng.word_index)+1
-train_with_adam(train_dataset, eng_word_count, pt_word_count)
+pt_word_count = len(tokenizer_pt.word_index)+1
+
+train_base(train_dataset, eng_word_count, pt_word_count, epochs = 1)
 
